e03ea458c64f973891dd714f5e4a7c6ba2e02b1b
vzlat <vzlat@VIKI01>
2023-08-23 12:17:58 +0300

    almost_done

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -27,16 +27,47 @@ export default function Modal() {
           <div onClick={toggleModal} className="overlay"></div>
           <div className="modal-content">
             <h2>Hello Modal</h2>
+
+        <label className='flex flex-col'>
+          <label className='flex flex-row'>  
             <img
             src={construction}
             alt='test'
-            className='w-full h-full object-cover'
+            className='w-1/2 h-1/2 object-cover'
           />
           <img
             src={tripguide}
             alt='test'
-            className='w-full h-full object-cover'
+            className='w-1/2 h-1/2 object-cover'
           />
+         </label>
+
+         <label className='flex flex-row'> 
+           <img
+            src={construction}
+            alt='test'
+            className='w-1/2 h-1/2 object-cover'/>
+            <img
+            src={tripguide}
+            alt='test'
+            className='w-1/2 h-1/2 object-cover'
+          /> 
+          </label>
+          
+          <label className='flex flex-row'> 
+           <img
+            src={construction}
+            alt='test'
+            className='w-1/2 h-1/2 object-cover'/>
+            <img
+            src={tripguide}
+            alt='test'
+            className='w-1/2 h-1/2 object-cover'
+          /> 
+          </label>
+
+        </label>
+          
             <button className="close-modal" onClick={toggleModal}>
               CLOSE
             </button>
