8a39358cf9bea56f8cf6e3e7bf4e2223f05ae6e0
vzlat <vzlat@VIKI01>
2023-09-14 18:06:50 +0300

    problems

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ const App = () => {
           <Contact />
           <StarsCanvas />
         </div>
-        <Comments />
       </div>
     </BrowserRouter>
   );
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -8,7 +7,6 @@ import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 import Modal from "./Modal";
-
 
 const ProjectCard = ({
   index,
@@ -19,44 +17,26 @@ const ProjectCard = ({
   source_code_link,
 }) => {
   return (
-
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <div
-
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
-      >
-        <div className='relative w-full h-[230px]'>
-          <img
-            src={image}
-            alt='project_image'
-            className='w-full h-full object-cover rounded-2xl'
-            
-          />
-          
-          <div
-              className='transparent w-10 h-10 rounded-full flex justify-center items-center cursor-pointer z-130'
-            >
-              <Modal />
-            </div>
-
-            </div>
-
-        <div className='mt-5'>
-          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)} className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'>
+      <div className='relative w-full h-[230px]'>
+        <img src={image} alt='project_image' className='w-full h-full object-cover rounded-2xl' />
+        <div className='transparent w-10 h-10 rounded-full flex justify-center items-center cursor-pointer z-130'>
+          <Modal />
         </div>
+      </div>
 
-        <div className='mt-4 flex flex-wrap gap-2'>
-          {tags.map((tag) => (
-            <p
-              key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
-            >
-              #{tag.name}
-            </p>
-          ))}
-        </div>
-        </div>
+      <div className='mt-5'>
+        <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+        <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+      </div>
+
+      <div className='mt-4 flex flex-wrap gap-2'>
+        {tags.map((tag) => (
+          <p key={`${name}-${tag.name}`} className={`text-[14px] ${tag.color}`}>
+            #{tag.name}
+          </p>
+        ))}
+      </div>
     </motion.div>
   );
 };
@@ -69,19 +49,17 @@ const Works = () => {
         <h2 className={`${styles.sectionHeadText} `}>Проекти</h2>
       </motion.div>
 
-      <div
-              className='transparent w-10 h-10 rounded-full flex justify-center items-center cursor-pointer z-130'
-            >
-              <Modal />
-            </div>
+      <div className='transparent w-10 h-10 rounded-full flex justify-center items-center cursor-pointer z-130'>
+        <Modal />
+      </div>
 
       <div className='w-full flex'>
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-[#000000] text-[19px] max-w-3xl leading-[30px]'
         >
-          Представяме ви няколко примера за нашата работа. Всеки проект е описан накратко и съдържа връзки, 
-          за да можете да получите пълна представа за това, което правим. Тези проекти отразяват 
+          Представяме ви няколко примера за нашата работа. Всеки проект е описан накратко и съдържа връзки,
+          за да можете да получите пълна представа за това, което правим. Тези проекти отразяват
           нашите способности като креативност, ефективност, опит и много други.
         </motion.p>
       </div>
